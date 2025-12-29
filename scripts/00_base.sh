c5c92692a7835421fccdba1ace47f4f5fa944026
Rafie Gunawan <rafiegunawan.im@gmail.com>
2025-12-29 16:50:54 +0700

    update: base installer

--- scripts/00_base.sh
+++ scripts/00_base.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "🧱 [00] Base system setup"
+echo "💻 Installing dependencies..."
+apt update && apt upgrade -y
+apt install -y git python3-pip python3-venv \
+    libldap2-dev libpq-dev libsasl2-dev wget curl ufw
 
-# Timezone
-timedatectl set-timezone Asia/Jakarta
+# wkhtmltopdf
+WKDL="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb"
+wget $WKDL -O /tmp/wkhtmltox.deb
+dpkg -i /tmp/wkhtmltox.deb || apt-get install -f -y
 
-# Update OS
-apt update -y
-apt upgrade -y
-
-# Basic packages
-apt install -y \
-  curl \
-  wget \
-  git \
-  ca-certificates \
-  gnupg \
-  lsb-release \
-  software-properties-common \
-  unzip \
-  ufw
-
-# Firewall
-ufw allow OpenSSH
-ufw allow 80
-ufw allow 443
-ufw --force enable
-
-echo "✅ Base system ready"
+# libssl1.1
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb -O /tmp/libssl1.1.deb
+dpkg -i /tmp/libssl1.1.deb || apt-get install -f -y
